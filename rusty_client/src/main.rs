83d099ba1fd187e9a509d4a39e016bba19ce77f7
Lasse <206079734+Lasse1926@users.noreply.github.com>
2026-04-26 11:21:57 +0200

    Arch(Client): switch to tokio async

--- rusty_client/src/main.rs
+++ rusty_client/src/main.rs
@@ -1,30 +1,67 @@
-use std::thread;
-use std::sync::mpsc::{self, Sender, Receiver};
-use reqwest::{header::USER_AGENT, Client};
-use serde::Deserialize;
+use std::collections::HashMap;
+use std::sync::{Arc,Mutex};
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
     let _ = eframe::run_native("My egui App", native_options, Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))));
 }
 
-// #[derive(Deserialize, Debug)]
-// struct Todo {
-//   userId: i32,
-//   id: i32,
-//   title: String,
-//   completed: bool
-// }
+#[derive(Hash, Eq, PartialEq, Clone, Copy,Debug)]
+enum RequestId {
+    Increase,
+    Decrease,
+    RequestData,
+}
 
+#[derive(Debug)]
 enum AppMessage {
     Msg(String),
     Error(String),
 }
 
+struct RequestState {
+    loading: bool,
+    result: Arc<Mutex<Option<AppMessage>>>,
+}
+
+impl RequestState {
+    fn new() -> Self {
+        Self {
+            loading: false,
+            result: Arc::new(Mutex::new(None)),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct ApiClient{
+    client:reqwest::Client,
+}
+
+impl ApiClient {
+    async fn request_data(&self) -> Result<AppMessage, reqwest::Error> {
+        let res = self.client.get("http://localhost:8080/counter").send().await?;
+        let text = res.text().await?;
+        Ok(AppMessage::Msg(text))
+    }
+
+    async fn increase_counter(&self) -> Result<AppMessage, reqwest::Error> {
+        let res = self.client.put("http://localhost:8080/counter/increase").send().await?;
+        let text = res.text().await?;
+        Ok(AppMessage::Msg(text))
+    }
+
+    async fn decreas_counter(&self) -> Result<AppMessage, reqwest::Error> {
+        let res = self.client.put("http://localhost:8080/counter/decrease").send().await?;
+        let text = res.text().await?;
+        Ok(AppMessage::Msg(text))
+    }
+}
+
 struct MyEguiApp {
-    client:reqwest::blocking::Client,
-    tx: Sender<AppMessage>,
-    rx: Receiver<AppMessage>,
+    api_client: ApiClient,
+    runtime: tokio::runtime::Runtime,
+    requests: HashMap<RequestId, RequestState>,
 
 }
 
@@ -34,91 +71,74 @@ impl MyEguiApp {
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
-        let (tx, rx) = std::sync::mpsc::channel::<AppMessage>();
         Self { 
-            client: reqwest::blocking::Client::new(),
-            tx,
-            rx, 
+            api_client: ApiClient { client: reqwest::Client::new() }, 
+            runtime: tokio::runtime::Runtime::new().unwrap(),
+            requests: HashMap::new(),
         }
     }
-    fn request_data(&self) {
-        let tx = self.tx.clone();
-        let client = self.client.clone();
-        thread::spawn(move || {
-            let res = client.get("http://localhost:8080/counter").send()
-                        .and_then(|r| r.text());
 
-            match res {
-                Ok(msg) => {
-                    let _ = tx.send(AppMessage::Msg(msg));
-                }
-                Err(e) => {
-                    let _ = tx.send(AppMessage::Error(e.to_string()));
-                }
-            }
+    fn state(&mut self, id: RequestId) -> &mut RequestState {
+        self.requests.entry(id).or_insert_with(RequestState::new)
+    }
+
+    fn spawn_request<F>(&mut self, id: RequestId, fut: F)
+    where
+        F: std::future::Future<Output = AppMessage> + Send + 'static,
+    {
+        let state = self.state(id);
+        state.loading = true;
+        let result = state.result.clone();
+
+        self.runtime.spawn(async move {
+            let msg = fut.await;
+            *result.lock().unwrap() = Some(msg);
         });
     }
 
-    fn increase_counter(&self) {
-        let tx = self.tx.clone();
-        let client = self.client.clone();
-        thread::spawn(move || {
-            let res = client.put("http://localhost:8080/counter/increase").send()
-                        .and_then(|r| r.text());
-
-            match res {
-                Ok(msg) => {
-                    let _ = tx.send(AppMessage::Msg(msg));
-                }
-                Err(e) => {
-                    let _ = tx.send(AppMessage::Error(e.to_string()));
-                }
-            }
-        });
-    }
-
-    fn decreas_counter(&self) {
-        let tx = self.tx.clone();
-        let client = self.client.clone();
-        thread::spawn(move || {
-            let res = client.put("http://localhost:8080/counter/decrease").send()
-                        .and_then(|r| r.text());
-
-            match res {
-                Ok(msg) => {
-                    let _ = tx.send(AppMessage::Msg(msg));
-                }
-                Err(e) => {
-                    let _ = tx.send(AppMessage::Error(e.to_string()));
-                }
-            }
-        });
-    }
 }
 
 impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        while let Ok(msg) = self.rx.try_recv() {
-            match msg {
-                AppMessage::Msg(msg) => {
-                    println!("{}",msg);
-                }
-                AppMessage::Error(e) => {
-                }
+        for (id, state) in &mut self.requests {
+            if let Some(msg) = state.result.lock().unwrap().take() {
+                state.loading = false;
+                println!("Request {:?} finished: {:?}", id, msg);
             }
         }
         egui::CentralPanel::default().show(ctx, |ui| {
-            if ui.button("send request").clicked() {
-                self.request_data();
+            let state = self.state(RequestId::RequestData);
+
+            if state.loading {
+                ui.add_enabled(false, egui::Button::new("Request Data..."));
+            } else if ui.button("Request Data").clicked() {
+                let api = self.api_client.clone();
+                self.spawn_request(RequestId::RequestData, async move {
+                    api.request_data().await.unwrap_or(AppMessage::Error("failed".into()))
+                });
             }
 
             ui.vertical(|ui|{
-                if ui.button("Increase").clicked() {
-                    self.increase_counter();
+                let state = self.state(RequestId::Increase);
+
+                if state.loading {
+                    ui.add_enabled(false, egui::Button::new("Increasing..."));
+                } else if ui.button("Increase").clicked() {
+                    let api = self.api_client.clone();
+                    self.spawn_request(RequestId::Increase, async move {
+                        api.increase_counter().await.unwrap_or(AppMessage::Error("failed".into()))
+                    });
                 }
 
-                if ui.button("Decrease").clicked() {
-                    self.decreas_counter();
+                let state = self.state(RequestId::Decrease);
+
+                if state.loading {
+                    ui.add_enabled(false, egui::Button::new("Decreasing..."));
+                } else if ui.button("Decrease").clicked() {
+                    let api = self.api_client.clone();
+                    self.spawn_request(RequestId::Decrease, async move {
+                        api.decreas_counter().await.unwrap_or(AppMessage::Error("failed".into()))
+                    });
                 }
             })
         });
